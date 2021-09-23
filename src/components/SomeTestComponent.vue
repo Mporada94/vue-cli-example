38a8366359be70c6c3a4caf7deeffb8a8c5873a7
mporada <mateusz.porada.ext@nokia.com>
2021-09-23 11:58:35 +0200

    initial

---
+++ src/components/SomeTestComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <label>Kolor tekstu został zmieniony</label>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+       
+    },
+    beforeCreate() {
+        console.log('beforeCreate');
+    },
+    created() {
+        console.log('created');
+    },
+    beforeMount() {
+        console.log('beforeMount');
+    },
+    mounted() {
+        console.log("mounted");
+    },
+    // computed: {
+    //     console.log("computed");
+    // },
+    beforeDestroy() {
+        console.log('beforeDestroy');
+    },
+    destroyed() {
+        console.log("destroyed");
+    },
+    methods: {
+        
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+    <div class="row text-center">
+        <h3>Clicked: {{ $store.state.count }} times, count is {{ getEvenOrOdd }}.</h3>
+        <button class="btn btn-success" @click="increment">+</button>
+        <button class="btn btn-danger" @click="decrement">-</button>
+        <button class="btn" @click="incrementIfOdd">Increment if odd</button>
+    </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+export default {
+    computed: mapGetters([
+        'getEvenOrOdd'
+    ]),
+    
+    methods: mapActions([
+        'increment',
+        'decrement',
+        'incrementIfOdd'
+    ])
+}
+
+</script>
+
+<style scoped>
+
+</style>
